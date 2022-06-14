a0b1494d3a766bafed0deb725b64b69d010b4701
Trenisha Goslee <60710670+tgoslee@users.noreply.github.com>
2022-06-14 13:19:37 -0400

    set up cat and catList component

---
+++ src/components/Cat.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Cat = () => {
+    
+    return (
+        <li>
+            <h3>
+                Name of Cat
+            </h3>
+            <button> 
+                Pet Cat 
+            </button>
+        </li>
+    );
+}
+
+export default Cat;
---
+++ src/components/CatList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Cat from './Cat';
+
+const CatList = () => {
+    return (
+        <ul>
+            <Cat />
+            <Cat />
+            <Cat />
+        </ul>
+    );
+};
+
+export default CatList;
