c226418596b65f11d75d4eb70fcc1b80e7919d23
Danielle <dmbecker24@gmail.com>
2022-06-16 10:30:50 -0700

    Add proptypes to CatList component

--- src/components/CatList.js
+++ src/components/CatList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Cat from "./Cat";
+import PropTypes from "prop-types";
 
 export const CatList = ({ cats }) => {
   return (
@@ -10,4 +11,13 @@ export const CatList = ({ cats }) => {
       })}
     </ul>
   );
+};
+
+CatList.propTypes = {
+  cats: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      chipNumber: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
