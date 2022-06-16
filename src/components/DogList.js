3f127a4c84aa53730add7d8fd99617632a0f4ca2
Philomena Kelly <philomena.a.kelly@outlook.com>
2022-06-15 22:53:24 -0400

    updated dog component

--- src/components/DogList.js
+++ src/components/DogList.js
@@ -4,7 +4,7 @@ import Dog from "./Dog";
 export const DogList = ({ dogs }) => {
   return (
     <ul>
-      <h2>Pup Count: 3</h2>
+      <h2>Pup Count: 3</h2> 
       {dogs.map((dog) => {
         return <Dog name={dog.name} chipNum={dog.chipNumber} />;
       })}
